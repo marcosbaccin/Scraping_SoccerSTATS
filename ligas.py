e9cbe02361d9117b35d91ccc62d60829b2fff41d
Marcos Baccin <marcos.baccin@estudante.ifgoiano.edu.br>
2025-04-28 22:35:36 -0300

    Update ligas.py

--- ligas.py
+++ ligas.py
@@ -37,11 +37,10 @@ tabela = driver.find_element(By.XPATH, '//*[@id="btable"]/tbody')
 
 ligas = tabela.find_elements(By.TAG_NAME, 'tr')
 print(len(ligas))
-links = pd.DataFrame(columns=['League', '+1.5', '+2.5', '+3.5', 'BTS'])
+links = pd.DataFrame(columns=['League', '+1.5', '+2.5', 'BTS'])
 qtd_ligas = 0
 media_mtog = 0
 media_mttg = 0
-media_three = 0
 media_bts = 0
 
 i = 1
@@ -71,15 +70,6 @@ for l in ligas:
         except:
             #print("")
             pass
-    
-    try:
-        three = driver.find_element(By.XPATH, f'//*[@id="btable"]/tbody/tr[{i}]/td[13]').get_attribute('innerHTML').replace('%', '')
-
-        if three != '&nbsp;-&nbsp;':
-            media_three = media_three + int(three)
-    except:
-        #print("")
-        pass
 
     try:
         bts = driver.find_element(By.XPATH, f'//*[@id="btable"]/tbody/tr[{i}]/td[14]/font').get_attribute('innerHTML').replace('%', '')
@@ -97,7 +87,7 @@ for l in ligas:
         #link_final = link_pt1 + link_pt2
         #link_final = link_pt1
         
-        links.loc[len(links)] = [f"'{link}',", int(mtog), int(mttg), int(three), int(bts)]
+        links.loc[len(links)] = [f"'{link}',", int(mtog), int(mttg), int(bts)]
     except:
         #print("")
         pass
@@ -106,17 +96,14 @@ for l in ligas:
 
 media_mtog = media_mtog / qtd_ligas
 media_mttg = media_mttg / qtd_ligas
-media_three = media_three / qtd_ligas
 media_bts = media_bts / qtd_ligas
 
 print(f'Frequencia de +1.5 gols: {media_mtog:.2f}%')
 print(f'Frequencia de +2.5 gols: {media_mttg:.2f}%')
-print(f'Frequencia de +3.5 gols: {media_three:.2f}%')
 print(f'Frequencia de Ambas Marcam: {media_bts:.2f}%')
 
 links_mtog = links[links['+1.5'] > media_mtog][['League']]
 links_mttg = links[links['+2.5'] > media_mttg][['League']]
-links_three = links[links['+3.5'] > media_three][['League']]
 links_bts = links[links['BTS'] > media_bts][['League']]
 
 links_mtog.sort_values(['League'], ascending=[True], inplace=True)
@@ -125,11 +112,8 @@ links_mtog.to_csv("C:/VScode_projects/soccerstats/links_MTOG.csv", index=False)
 links_mttg.sort_values(['League'], ascending=[True], inplace=True)
 links_mttg.to_csv("C:/VScode_projects/soccerstats/links_MTTG.csv", index=False)
 
-links_three.sort_values(['League'], ascending=[True], inplace=True)
-links_three.to_csv("C:/VScode_projects/soccerstats/links_THREE.csv", index=False)
-
 links_bts.sort_values(['League'], ascending=[True], inplace=True)
 links_bts.to_csv("C:/VScode_projects/soccerstats/links_BTS.csv", index=False)
 
-links.drop(columns=['+1.5', '+2.5', '+3.5', 'BTS'], inplace=True)
+links.drop(columns=['+1.5', '+2.5', 'BTS'], inplace=True)
 links.to_csv("C:/VScode_projects/soccerstats/links.csv", index=False)
